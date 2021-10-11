be898005f9e54235f08d29e38452daec19756907
Harpalsinh <jadejaharpal14@gmail.com>
2021-10-11 11:47:07 +0530

    Profile Page done

--- src/theme/components/Tag.js
+++ src/theme/components/Tag.js
@@ -1,7 +1,14 @@
 export default {
+    parts: ["container", "label"],
     variants: {
         subtle: {
-            
+            container: {
+                bg: "brand.100",
+                color: "light.200"
+            }
         }
+    },
+    defaultProps: {
+        variant: "subtle"
     }
 }
